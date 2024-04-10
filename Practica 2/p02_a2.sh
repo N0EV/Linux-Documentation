b8b6327d5d564d3614143d35a62c364efc825519
noe <noevalladolidescudero@gmail.com>
2024-04-10 16:01:40 +0200

    ejercicio 2

--- Practica 2/p02_a2.sh
+++ Practica 2/p02_a2.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+# Para este script vamos a tener que crear las carpetas necesarias en el home (linux)
+echo "Vamos a ordenar las descaras"
+cd ~/Descargas
+# Aqui podriamos poner variables para las rutas pero yo no lo he hecho
+# otro dato es que en linux hay una variable que es $HOME que es el home
+# del usuario que lo este usando
+# Aqui vamos a coger todo lo que haya en descargas dado que hemos ido antes
+for [[ archivo in * ]]; do
+	extension=${archivo##*.}
+	case $extension in
+		jpg|jpeg|png)
+			mv $archivo ~/Imágenes;;
+		mp3|mp4)
+			mv $archivo ~/Música;;
+	esac
+done
