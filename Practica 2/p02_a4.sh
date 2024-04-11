1da5c281dbf26365d60e0dd7553e88f875a0dff8
noe <noevalladolidescudero@gmail.com>
2024-04-11 16:30:12 +0200

    Ejercicio 4 esta hecho

--- Practica 2/p02_a4.sh
+++ Practica 2/p02_a4.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-
+ruta="$HOME/copia/"
+echo "Recuerde indicar rutas absolutas"
+read -p "¿ Que directorio quiere hacer la copia de seguirdad ?" segur
+if [[ -d $segur ]]; then
+	echo "Vamos a crear la copia de seguridad"
+	if [[ -d $ruta ]]; then
+		mkdir $ruta
+	fi
+	tar -cvzf $ruta/CopiadeSeguridad.tar.gz $segur
+else
+	echo "El directorio no existe"
+	exit 1
+fi
+echo "Directorio comprimido"
