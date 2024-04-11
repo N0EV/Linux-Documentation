92d49d28542b337f4050a3afcd1d505ad75565ca
noe <noevalladolidescudero@gmail.com>
2024-04-11 15:55:56 +0200

    Ejercicio 3 esta echo

--- Practica 2/p02_a3.sh
+++ Practica 2/p02_a3.sh
@@ -3,13 +3,11 @@ cd ~/Documentos
 ls -l ~/Documentos
 
 for archivo in *; do
-	fecha_creacion=$(stat -c %Y "$archivo" | cut -d' ' -f1)
+	fecha_creacion=$(stat -c %y "$archivo" | cut -d' ' -f1)
 	anio=$(date -d "$fecha_creacion" "+%Y")
 	mes=$(date -d "$fecha_creacion" "+%m")
-	dirdestino=$anio"_"$mes
-		if [[ -d $dirdestino ]]; then
-			mv $archivo $dirdestino
-		else
+	dirdestino="$anio"_"$mes"
+		if [[ ! -d $dirdestino ]]; then
 			mkdir $dirdestino
 		fi
 	mv $archivo $dirdestino
