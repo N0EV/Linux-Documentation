eb01b6ff2d0dc64553e166aa57fd5aaa31815ac7
noe <noevalladolidescudero@gmail.com>
2024-04-11 17:04:53 +0200

    Practica 2 estan hechas

--- Practica 2/p02_a5.sh
+++ Practica 2/p02_a5.sh
@@ -2,8 +2,8 @@
 if [[ $# -ne 1 ]];then
 	echo "Uso: <nombre de archivo de texto>"
 fi
-
+archivo=$1
 if [[ -f $1 ]]; then
-	npalabras=$(wc -w < "$1")
+	npalabras=$(wc -w < "$archivo")
 fi
 echo $npalabras
