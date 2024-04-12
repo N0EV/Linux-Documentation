cb77b8ae3cfcb78c2d2dd09b771792f55b6b81e4
noe <noevalladolidescudero@gmail.com>
2024-04-12 20:54:17 +0200

    Actualizado

--- Practica 3/p03_a3.sh
+++ Practica 3/p03_a3.sh
@@ -12,6 +12,9 @@ id_grupo_primario=$(grep "^$grupo_primario:" /etc/group | cut -d: -f3)
 usuarios=$(grep -E "^[^:]+:[^:]+:[^:]+:$id_grupo_primario" /etc/passwd | cut -d: -f1)
 if [[ -z $usuarios ]]; then
 	echo "No se encontraron usuarios en el grupo primario '$grupo_primario'."
+	exit 1
+fi
+
 for usua in $usuarios; do
 	sudo adduser "$usuarios" "$grupo_secundario"
 	echo "El usuario $usuario ha sido agregado al grupo secundario '$grupo_secuandario' correctamente"
