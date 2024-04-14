f40f7f205f600d067991585303671f16470e6c79
noe <noevalladolidescudero@gmail.com>
2024-04-14 20:06:52 +0200

    Actualizado

--- Practica 3/p03_a1.sh
+++ Practica 3/p03_a1.sh
@@ -4,9 +4,13 @@ if [[ $# -eq 0 ]]; then
 	echo "Uso: $0 <todos los usuarios que quiera>"
 	exit 1
 fi
+# Aqui lo que estamos haciendo es listar con un for todos
+# los "parametros" que nos a pasado el usuario
 for grupos in $@
 # y que haga lo siguiente
 do
+	# Esto lo que hace es sacarnos los grupos a los que esta cada "parametro"
+	# que nos ha dado el usuario
 	groups $grupos
 done
 # Y acaba aqui
