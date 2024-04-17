696bd82d56fa4d5287d52f1c750e9dabcfaf9a23
noe <noevalladolidescudero@gmail.com>
2024-04-17 10:38:05 +0200

    Acualizado

--- Practica 1/p01_a1.sh
+++ Practica 1/p01_a1.sh
@@ -2,9 +2,11 @@
 # $1 y $2 se recongen justo despues de hacer bash y el fichero
 # Aqui estos indicando que $1 y $2 que son dos variables
 # si $1 es mayor que $2
+
 if [[ $1 -gt $2 ]]; then
 	echo "$1 es mayor que $2"
 # Si $1 es igual que $2
+
 elif [[ $1 -eq $2 ]]; then
 	echo "$1 es igual que $2"
 # Si no lo son entonces deducimos que $2
